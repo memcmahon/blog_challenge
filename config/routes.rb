f3254880718b0aaa6d5c2cef682ef084ae4ece64
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-06-01 11:11:37 -0600

    Adds edit and destroy post routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'welcome#index'
 
   resources :users, only: [:new, :create]
-  resources :posts, only: [:new, :create]
+  resources :posts, only: [:new, :create, :edit, :update, :destroy]
   get '/login', to: 'sessions#new'
   get '/logout', to: 'sessions#destroy'
   post '/login', to: 'sessions#create'
