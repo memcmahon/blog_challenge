2a54d8cd391eed81d564eb84c4e70ee29373dd21
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-06-01 11:00:18 -0600

    Adds spec for post edit and delete

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -69,5 +69,53 @@ describe "As a user" do
       expect(page).to have_content('Test Title')
       expect(page).to have_content('test post body')
     end
+
+    it "they can edit an existing post" do
+      user = User.create!(email: 'test@example.com', password: 'test', first_name: 'Megan', last_name: 'Test')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/'
+      click_on 'New Post'
+
+      fill_in 'post_title', with: 'Test Title'
+      fill_in 'post_body', with: 'test post body'
+      click_on 'Create Post'
+
+      expect(current_path).to eq('/')
+      expect(page).to have_content('Test Title')
+      expect(page).to have_content('test post body')
+
+      click_on 'Edit'
+
+      fill_in 'post_title', with: 'Test Title'
+      fill_in 'post_body', with: 'test post body updated'
+      click_on 'Update Post'
+
+      expect(current_path).to eq('/')
+      expect(page).to have_content('Test Title')
+      expect(page).to have_content('test post body updated')
+    end
+
+    it "they can delete an existing post" do
+      user = User.create!(email: 'test@example.com', password: 'test', first_name: 'Megan', last_name: 'Test')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit '/'
+      click_on 'New Post'
+
+      fill_in 'post_title', with: 'Test Title'
+      fill_in 'post_body', with: 'test post body'
+      click_on 'Create Post'
+
+      expect(current_path).to eq('/')
+      expect(page).to have_content('Test Title')
+      expect(page).to have_content('test post body')
+
+      click_on 'Delete'
+
+      expect(current_path).to eq('/')
+      expect(page).to_not have_content('Test Title')
+      expect(page).to_not have_content('test post body')
+    end
   end
 end
