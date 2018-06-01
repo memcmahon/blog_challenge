4552c1e203daeed96a9c57de3b9a64e12a6dcfcc
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-06-01 11:47:05 -0600

    Updates model specs for comment

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,6 @@ describe User, type: :model do
     it {should validate_uniqueness_of(:email)}
     it {should validate_presence_of(:password)}
     it {should have_many(:posts)}
+    it {should have_many(:comments)}
   end
 end
