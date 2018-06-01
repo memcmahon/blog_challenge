eb676660c865399555f925f91d33b5e0e57a10c3
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-06-01 11:46:50 -0600

    Adds feature spec for users adding comments

--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
-describe 'As a visitor' do
+describe 'As a user' do
   describe 'they visit the blog' do
     it 'they can leave a comment on a post' do
       user = User.create!(email: 'test@example.com', password: 'test', first_name: 'Megan', last_name: 'Test')
       post = user.posts.create(title: 'Test Title', body: 'Test post body')
 
+      other_user = User.create!(email: 'othertest@example.com', password: 'test', first_name: 'Molly', last_name: 'Test')
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(other_user)
+
       visit '/'
 
       click_on 'Comment'
 
-      fill_in 'body', with: 'trolleys are great'
+      fill_in 'comment_body', with: 'trolleys are great'
       click_on 'Create Comment'
 
       expect(current_path).to eq('/')
