c1812a4b9a336d1ffbd8bf3e3cd56631f906317a
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-06-01 10:56:15 -0600

    Adds posts to the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180601154820) do
+ActiveRecord::Schema.define(version: 20180601164041) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -22,4 +29,5 @@ ActiveRecord::Schema.define(version: 20180601154820) do
     t.string "last_name"
   end
 
+  add_foreign_key "posts", "users"
 end
